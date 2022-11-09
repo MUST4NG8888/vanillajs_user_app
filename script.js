ff087311d6b29502fd1645f83488dea515fe7904
Drew <andrew.lendvai@gmail.com>
2022-11-09 11:17:13 +0100

    Not pretty, but working

--- script.js
+++ script.js
@@ -5,11 +5,37 @@ appState = {
   data: [],
 };
 
-const searchData = (e) => {
-  let searchInput = e.target.value;
-  const result = appState.data.find((user) => user.login == `${searchInput}`);
-console.log(result)
-};
+// const searchData = (e) => {
+//   let searchInput = e.target.value;
+//   // const result = appState.data.find((user) => user.login == `${searchInput}`);
+//     const test = Object.values(appState.data[0]).includes(`${searchInput}`);
+// console.log(test)
+// };
+
+
+const searchData = (e) =>{
+  const filterString = e.target.value
+   const datas = appState.data;
+   filteredData = datas.filter(function (data){
+       let testData ="";
+       for(let i=0;i<filterString.length;i++){
+           testData +=data.login[i]
+           if(testData===filterString){
+               return data
+           }
+       }
+   })
+   wrapper.innerHTML="";
+   if(filteredData.length ===0 && e.target.value.length !=0){
+       wrapper.innerHTML="Nothing found"
+   }
+   if(e.target.value.length ===0){
+    renderUsers(appState.data)
+   }
+   renderUsers(filteredData)
+console.log(filteredData);
+}
+
 
 
 const requestData = async () => {
@@ -18,8 +44,9 @@ const requestData = async () => {
   appState.data = data;
 };
 
-const renderUsers = () => {
-  for (let i = 0; i < appState.data.length; i++) {
+const renderUsers = (data) => {
+  
+  for (let i = 0; i < data.length; i++) {
     let userBox = document.createElement("div");
     userBox.id = "userbox";
     let userImg = document.createElement("img");
@@ -42,10 +69,10 @@ const renderUsers = () => {
     userBox.append(rank);
     userBox.append(admin);
 
-    userImg.src = appState.data[i].avatar_url;
-    userName.innerText = appState.data[i].login;
-    rank.innerText = appState.data[i].type;
-    admin.innerText = appState.data[i].site_admin;
+    userImg.src = data[i].avatar_url;
+    userName.innerText = data[i].login;
+    rank.innerText = data[i].type;
+    admin.innerText = data[i].site_admin;
 
     app.append(userBox);
 
@@ -72,7 +99,6 @@ const renderUsers = () => {
 const init = async () => {
   await requestData();
   console.log(appState.data);
-  renderUsers();
   search.addEventListener("input", (e) => {
     searchData(e);
   });
